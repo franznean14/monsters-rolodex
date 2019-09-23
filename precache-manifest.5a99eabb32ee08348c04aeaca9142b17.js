d31d50bfba4c8e30b1de01c8aaa9f9ce2457cb6b
Franz Casabuena <franzneandra@gmail.com>
2019-09-23 15:03:04 +0800

    Updates

---
+++ precache-manifest.5a99eabb32ee08348c04aeaca9142b17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f516ee9921497194d57d077f86c6998",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f19c412a9670435e2240",
+    "url": "/monsters-rolodex/static/css/main.46558737.chunk.css"
+  },
+  {
+    "revision": "d3ef79087afc70e780c5",
+    "url": "/monsters-rolodex/static/js/2.2d84b1ef.chunk.js"
+  },
+  {
+    "revision": "f19c412a9670435e2240",
+    "url": "/monsters-rolodex/static/js/main.d0669c9d.chunk.js"
+  },
+  {
+    "revision": "5f535753dcec63ef3476",
+    "url": "/monsters-rolodex/static/js/runtime~main.7352cc78.js"
+  }
+]);
